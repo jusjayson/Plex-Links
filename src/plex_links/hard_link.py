32ba9cc4d3db28880b980b314cf35611db811b1d
Jayson Dorsett <ja_dorsett@jdorsett.com>
2023-02-14 14:16:26 -0500

    chore: make a folder for storing hardlinks

--- src/plex_links/hard_link.py
+++ src/plex_links/hard_link.py
@@ -5,3 +5,5 @@ def hard_link_dir(starting_dir: str):
     starting_dir = Path(starting_dir)
     if not starting_dir.exists():
         raise ValueError(f"Starting dir: {starting_dir} does not exist")
+    links_dir = Path(f"{str(starting_dir)}/hard_links_links")
+    links_dir.mkdir()

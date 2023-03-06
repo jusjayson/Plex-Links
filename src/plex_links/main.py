d7a106c7ca3958470ac86293e54616a2b4660865
Jayson Dorsett <ja_dorsett@jdorsett.com>
2023-03-06 10:52:10 -0500

    chore: add print statements to main

--- src/plex_links/main.py
+++ src/plex_links/main.py
@@ -3,10 +3,13 @@ from plex_links.hard_link import hard_link_dir
 from plex_links.unrar import unrar
 
 if sys.argv[1] == "hard_link_dir":
+    print("HARD LINKING DIR")
     hard_link_dir(sys.argv[2])
 
 elif sys.argv[1] == "hard_link_mkvs":
+    print("HARD LINKING MKVS")
     hard_link_dir(sys.argv[2])
 
 elif sys.argv[1] == "unrar_dir":
+    print("EXTRACTING DIR")
     unrar(sys.argv[2])

caa4afb968efac4dd0320ddf8d4703974fa7750b
Jayson Dorsett <ja_dorsett@jdorsett.com>
2023-03-06 10:50:21 -0500

    fix: unrar should take str as arg

--- src/plex_links/unrar.py
+++ src/plex_links/unrar.py
@@ -6,9 +6,12 @@ from pathlib import Path
 # for EPISODE in $(seq 13 14); do cd South.Park.S${SEASON}E${EPISODE}*/ && rar e *.rar && mv *.mkv ../hard_links_links/South.Park.S${SEASON}E${EPISODE}*/ && rm ../hard_links_links/South.Park.S${SEASON}E${EPISODE}*/*.r* && cd ..; done
 
 
-def unrar(dir: Path):
+def unrar(dir: str):
+    dir = Path(dir)
+    print("CHECKING", dir)
     for file_or_folder in dir.iterdir():
         if file_or_folder.is_dir():
             unrar(file_or_folder)
         elif "rar" in file_or_folder.suffix:
+            print("EXTRACTING", file_or_folder)
             os.system(f"rar e {file_or_folder}")
